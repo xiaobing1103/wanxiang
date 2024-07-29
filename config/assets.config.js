620d06c10a0378e694fc5fee69fdd737c67d81db
王茂全 <390351823@qq.com>
2024-07-29 15:04:29 +0800

    完成功能库静态页面

--- config/assets.config.js
+++ config/assets.config.js
@@ -4,5 +4,12 @@ const getUrl = (url) =>{
 }
 export default{
 	//我的页面
-	vipCard:getUrl('/2024/07/29/396b53c790a06edd99dd4676c9066bd8.svg')
+	vipCard:getUrl('/2024/07/29/396b53c790a06edd99dd4676c9066bd8.svg'),
+	// 功能页面
+	ppt_cover:getUrl('/2024/07/29/ae8a249b18293065506c5c18f9bf4802.svg'),
+	aixz_cover:getUrl('/2024/07/29/d43cefcff1e8a7c8c15ff4d0d3e6e391.svg'),
+	gzzj_cover:getUrl('/2024/07/29/aa9686df0e44172525901ab0a23ca56d.svg'),
+	swdt_cover:getUrl('/2024/07/29/55725ecb144c844178dbb1c178c75cca.svg'),
+	wzyh_cover:getUrl('/2024/07/29/a17dca27807bb8f048a6c963563da670.svg'),
+	wddh_cover:getUrl('/2024/07/29/23319b9224489ef905f0e677fe0d9a6e.svg')
 }
--- pages/function/index.vue
+++ pages/function/index.vue
@@ -1,17 +1,78 @@
 <template>
-	<view class="content">
-		<CommonHeader />
-		功能页面
-		<CommonTabbar :indexValue="3"/>
-	</view>
+	<z-paging :show-scrollbar="false">
+		<template #top>
+			<CommonHeader />
+		</template>
+		<view class="body">
+			<MenuCardItem @click="toPage(item)" v-for="(item,index) in menuList" :key="index" :data="item"/>
+		</view>
+		<template #bottom>
+			<CommonTabbar :indexValue="3"/>
+		</template>
+	</z-paging>
 </template>
 
-<script setup>
-	import api from '@/api/api.ts'
+<script setup lang="ts">
 	import CommonTabbar from '@/components/CommonTabbar.vue'
 	import CommonHeader from '@/components/CommonHeader.vue'
-
-	api.login().then(res => {
-		console.log(res)
-	})
+	import MenuCardItem from './comoonents/menuItem.vue'
+	import {useGlobalProperties} from '@/hooks/useGlobalHooks'
+	
+	import  type {MenuItem} from './type.ts'
+	const {$assets} = useGlobalProperties()
+	
+	const menuList = [
+		{
+			cover:$assets.ppt_cover,
+			title:'一键生成PPT',
+			description:'海量PPT末班任你选择',
+			path:'/pages/function/subPage/ppt/index'
+		},
+		{
+			cover:$assets.aixz_cover,
+			title:'AI长论文写作',
+			description:'千字大纲，万字毕业课题论文',
+			path:''
+		},
+		{
+			cover:$assets.gzzj_cover,
+			title:'AI工作总结',
+			description:'超实用,AI帮写周报月报总结',
+			path:''
+		},
+		{
+			cover:$assets.swdt_cover,
+			title:'AI思维导图',
+			description:'理解重点,生成可视化思维导图',
+			path:''
+		},
+		{
+			cover:$assets.wzyh_cover,
+			title:'AI文章纠正',
+			description:'AI帮你纠正语句错误,优化内容',
+			path:''
+		},
+		{
+			cover:$assets.wddh_cover,
+			title:'AI文档对话',
+			description:'通过Ai对话快速获取文档信息',
+			path:''			
+		}		
+	]
+	
+	const toPage = (item:MenuItem) =>{
+		console.log(item)
+		uni.navigateTo({
+			url:item.path
+		})
+	}
 </script>
+<style lang="scss" scoped>
+	.body{
+		display: grid;
+		grid-template-columns: repeat(2,1fr);
+		gap: 20rpx;
+		box-sizing: border-box;
+		padding: 30rpx;
+	}
+</style>
